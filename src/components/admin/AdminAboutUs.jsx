4234f8a89cacf1ab0f708eee247e34ca4fc23514
DevGeraldR <romerogerald22@gmail.com>
2024-03-10 19:46:00 +0800

    Fixed login bug

--- src/components/admin/AdminAboutUs.jsx
+++ src/components/admin/AdminAboutUs.jsx
@@ -1,67 +1,63 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Link } from "react-router-dom";
+import "../utils/style.css";
+import pegasusLogo from "../../assets/images/pegasus.png";
 
 const AdminAboutUs = () => {
-  const containerStyle = {
-    padding: '20px', // Optional: Add padding for better visual appearance
-  };
-
-  const textBackgroundStyle = {
-    backgroundColor: '#ddcdb35d', // Background color for the text
-    padding: '10px', // Optional: Add padding for better visual appearance
-    borderRadius: '8px', // Optional: Add border-radius for rounded corners
-  };
-
-  const transparentBackgroundStyle = {
-    backgroundColor: 'none', // Set background color to transparent
-    border: 'none', // Remove any border
-  };
-
   return (
     <div>
-      {/* Navigation Bar */}
       <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-    <div className="container">
-      <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link className="nav-link" to={'/admin'}>
-              Home
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to={'/AdminAboutUs'}>
-              About us
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+        <div className="container">
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link" to={"/admin"}>
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/AdminAboutUs"}>
+                  About us
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
 
       {/* Main Content */}
-      <div className="container" style={containerStyle}>
+      <div className="container containerStyle">
         <div className="title text-left">
           <h1 className="text-uppercase my-5">About Us</h1>
         </div>
         <div className="card mb-3">
           <div className="row">
             <div className="col-md-4">
-              <img src="../src/assets/images/pegasus.png" alt="Pegasus Logo" className="img-fluid" />
+              <img src={pegasusLogo} alt="" width={400} height={400} />
             </div>
             <div className="col-md-8">
               <div className="card-body">
                 <h3 className="card-title">Thesis Group</h3>
-                <p style={textBackgroundStyle} className="text-align-justify">
-                  Pegasus group has developed a web-based application for the Thesis Project Defense of the College of Information Technology and
-                  Communication at the University of Batangas. The system is accessible through any forms of technology that has an internet connection
-                  and allows panelists and advisers to evaluate the graduating students and their Thesis/Capstone Projects through an innovative way
-                  of grading. The system was developed using Agile Development Methodology to simplify the software development scheme of the system.
-                  The system was evaluated based on ISO 9126 standard which showed that the criteria on efficiency ranked as the highest, followed by
-                  the functionality and maintainability, criteria for portability and lastly criteria for the reliability and usability of the system
-                  ranks as the lowest. The result of the evaluation of Online Thesis/ Capstone Project Defense Evaluation System showed that the developed
-                  system achieved its functional requirements in applying the modern way of evaluating through online.
+                <p className="text-align-justify textBackgroundStyle">
+                  Pegasus group has developed a web-based application for the
+                  Thesis Project Defense of the College of Information
+                  Technology and Communication at the University of Batangas.
+                  The system is accessible through any forms of technology that
+                  has an internet connection and allows panelists and advisers
+                  to evaluate the graduating students and their Thesis/Capstone
+                  Projects through an innovative way of grading. The system was
+                  developed using Agile Development Methodology to simplify the
+                  software development scheme of the system. The system was
+                  evaluated based on ISO 9126 standard which showed that the
+                  criteria on efficiency ranked as the highest, followed by the
+                  functionality and maintainability, criteria for portability
+                  and lastly criteria for the reliability and usability of the
+                  system ranks as the lowest. The result of the evaluation of
+                  Online Thesis/ Capstone Project Defense Evaluation System
+                  showed that the developed system achieved its functional
+                  requirements in applying the modern way of evaluating through
+                  online.
                 </p>
               </div>
             </div>
--- src/components/visitor/Login.jsx
+++ src/components/visitor/Login.jsx
@@ -2,9 +2,8 @@ import React, { useState, useEffect } from "react";
 import "../utils/style.css";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import logo from '../../assets/images/CHATNB_LOGO.png'
-import { Link } from 'react-router-dom';
-
+import logo from "../../assets/images/CHATNB_LOGO.png";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   const [values, setValues] = useState({
@@ -33,11 +32,11 @@ const Login = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
     axios
-      .post("http://localhost:3000/admin/admin_login", values)
+      .post("http://localhost:3000/visitor/visitor_login", values)
       .then((result) => {
         if (result.data.loginStatus) {
-          localStorage.setItem("admin_valid", true);
-          navigate("/admin_dashboard");
+          localStorage.setItem("valid", true);
+          navigate("/dashboard");
         } else {
           setError(result.data.Error);
         }
@@ -46,67 +45,65 @@ const Login = () => {
   };
 
   return (
-    <div className='Login'>
-    <div className='logo'>
-    <img src={logo} alt="" width={600} height={600}/>
-    </div>  
-  <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-    <div className="container">
-      <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link className="nav-link" to={'/'}>
-              Home
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to={'/AboutUs'}>
-              About us
-            </Link>
-          </li>
-        </ul>
+    <div className="Login">
+      <div className="logo">
+        <img src={logo} alt="" width={600} height={600} />
+      </div>
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link" to={"/"}>
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/AboutUs"}>
+                  About us
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div className="auth-inner">
+        <form onSubmit={handleSubmit}>
+          <h1 className="ChatNB">Chat NB</h1>
+          <h3 className="Text1">Artificial Intelligence Application</h3>
+          <h3 className="Text1">to talk to Notable Batanguenos</h3>
+
+          <h6 className="Text2">Welcome! Please login to your account</h6>
+          <div className="mb-3">
+            <label>Username</label>
+            <input
+              type="text"
+              name="name"
+              placeholder="Enter Username"
+              onChange={(e) =>
+                setValues({ ...values, username: e.target.value })
+              }
+            />
+          </div>
+          <div className="mb-3">
+            <label>Password</label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Enter Password"
+              onChange={(e) =>
+                setValues({ ...values, password: e.target.value })
+              }
+            />
+          </div>
+          <div className="d-grid">
+            <button type="submit" className="btn-primary">
+              Login
+            </button>
+          </div>
+        </form>
       </div>
     </div>
-  </nav>
-  <div className="auth-inner">
-    
-    <form onSubmit={handleSubmit}>
-    <h1 className='ChatNB'>Chat NB</h1>
-    <h3 className='Text1'>Artificial Intelligence Application</h3>
-    <h3 className='Text1'>to talk to Notable Batanguenos</h3>
-
-    <h6 className='Text2'>Welcome! Please login to your account</h6>
-    <div className="mb-3">
-      <label>Username</label>
-      <input
-        type="text"
-        name="name"
-        placeholder="Enter Username"
-        onChange={(e) =>
-          setValues({ ...values, username: e.target.value })
-        }
-      />
-    </div>
-    <div className="mb-3">
-      <label>Password</label>
-      <input
-          type="password"
-          name="password"
-          placeholder="Enter Password"
-          onChange={(e) =>
-            setValues({ ...values, password: e.target.value })
-          }
-      />
-    </div>
-    <div className="d-grid">
-      <button type="submit" className="btn-primary">
-        Login
-      </button>
-    </div>
-  </form>
-
-  </div>      
-  </div>
   );
 };
 
